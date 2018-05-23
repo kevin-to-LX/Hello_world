6ee0024a2ede1dbf4a2648e491670d4f4fa74d8d
jinyugai <18868876912@163.com>
2018-05-23 14:41:37 +0800

    Initial commit

---
+++ src/com/kevin/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.kevin.demo;
+
+/**
+ * Created by jinyugai on 2018/5/23.
+ */
+public class HelloWorld {
+    public static  void  main(String[] arg){
+        System.out.print("Hello World");
+    }
+}
